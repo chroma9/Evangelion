36a6ddd46a199b3e07df9caa9abe149b0ddf4e03
Chroma9 <64189171+chroma9@users.noreply.github.com>
2020-07-19 01:15:16 -0400

    Update interfaces.ts

--- source/interfaces.ts
+++ source/interfaces.ts
@@ -1,1 +1,29 @@
 export type Generic = {[key: string]: any};
+
+export interface UserData {
+    _id: string;
+    username: string;
+    discrim: string;
+    avatar: string;
+    eva: EVAMech;
+}
+
+export interface GuildData {
+    _id: string;
+    name: string;
+    icon: string;
+    region: string;
+}
+
+export interface EVAMech {
+    id: string;
+    name: string;
+    color: string;
+    atField: number;
+
+}
+
+export interface VoiceRegion {
+    name: string;
+    flag: string;
+}
